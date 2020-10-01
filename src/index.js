13a38c5e0f8ea8a8da1aebf9ce028d772f69f791
Sumer Cetin <sumercetin3@gmail.com>
2020-10-01 09:26:34 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,77 @@
+// Please get all people info from Star Wars API (https://swapi.dev)
+// and show the info as in the example
+// Dont forget to show a loading info for the users while preparing the data
+import swapi from "./request";
+const appDiv = document.querySelector("#app");
+const dataText = {
+  loadingText: "Loading...",
+  nameText: "Name",
+  heightText: "Height",
+  massText: "Mass",
+  homeWorldText: "Homeworld",
+  populationText: "population",
+  usedVehiclesText: "Used Vehicles",
+  usedStarshipsText: "Used Starships"
+};
+
+(async () => {
+  const loadingNode = document.createElement("p");
+  loadingNode.append(dataText.loadingText);
+  appDiv.appendChild(loadingNode);
+  const data = JSON.parse(await swapi.getData());
+  appDiv.removeChild(loadingNode);
+  populateApp(data);
+})();
+
+const createParagraph = (data, personDiv, text) => {
+  const node_p = document.createElement("p");
+  node_p.append(`${text}: ${data}`);
+  personDiv.appendChild(node_p);
+};
+
+const createParagraphForHomeworld = (data, personDiv, text, text2) => {
+  const node_p = document.createElement("p");
+  node_p.append(`${text}: ${data.name}, ${text2}: ${data.population}`);
+  personDiv.appendChild(node_p);
+};
+
+const createList = (dataArr, personDiv, text) => {
+  //create paragraph for list and append
+  const node_p = document.createElement("p");
+  node_p.append(text);
+  personDiv.appendChild(node_p);
+
+  //create unordered list
+  const node_ul = document.createElement("ul");
+
+  dataArr.forEach((data) => {
+    const node_li = document.createElement("li");
+    node_li.append(`${data.name}, model: ${data.model}`);
+
+    node_ul.appendChild(node_li);
+  });
+
+  personDiv.appendChild(node_ul);
+};
+
+const populateApp = (data) => {
+  data.forEach((person) => {
+    const personDiv = document.createElement("div");
+
+    createParagraph(person.name, personDiv, dataText.nameText);
+    createParagraph(person.height, personDiv, dataText.heightText);
+    createParagraph(person.mass, personDiv, dataText.massText);
+    createParagraphForHomeworld(
+      person.homeplanet,
+      personDiv,
+      dataText.homeWorldText,
+      dataText.populationText
+    );
+
+    createList(person.vehicles, personDiv, dataText.usedVehiclesText);
+    createList(person.starships, personDiv, dataText.usedStarshipsText);
+
+    appDiv.appendChild(personDiv);
+    appDiv.innerHTML += "<hr />";
+  });
+};
---
+++ src/request.js
@@ -0,0 +1,90 @@
+const NUM_PEOPLE = 15;
+
+const getData = async () => {
+  const dataArr = [];
+
+  try {
+    for (let i = 1; i <= NUM_PEOPLE; i++) {
+      const personData = await getPeopleData(i);
+
+      if (personData.species.length === 0) {
+        const homeworldData = await getHomeworldData(personData.homeworld);
+
+        const vehicleData = await getVehicleAndStarshipData(
+          personData.vehicles
+        );
+
+        const starshipData = await getVehicleAndStarshipData(
+          personData.starships
+        );
+
+        const person = {
+          name: `${personData.name}`,
+          height: `${personData.height}`,
+          mass: `${personData.mass}`,
+          homeplanet: {
+            name: `${homeworldData.name}`,
+            population: `${homeworldData.population}`
+          },
+          vehicles: vehicleData,
+          starships: starshipData
+        };
+        dataArr.push(person);
+      }
+    }
+
+    return JSON.stringify(dataArr);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const getPeopleData = async (index) => {
+  try {
+    const url = new URL(`${index}/`, "https://swapi.dev/api/people/");
+    const res = await fetch(url);
+    return res.json();
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const getHomeworldData = async (urlPar) => {
+  try {
+    const url = httpToHttps(urlPar);
+
+    const res = await fetch(url);
+    return res.json();
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const getVehicleAndStarshipData = async (urlArr) => {
+  try {
+    const resArr = [];
+    for (let i = 1; i <= urlArr.length; i++) {
+      const url = httpToHttps(urlArr[i - 1]);
+
+      const res = await fetch(url);
+      const data = await res.json();
+
+      const obj = {
+        name: data.name,
+        model: data.model
+      };
+      resArr.push(obj);
+    }
+    return resArr;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const httpToHttps = (url_p) => {
+  const url_https = new URL(url_p);
+  url_https.protocol = "https:";
+  return url_https;
+};
+
+export default { getData };
